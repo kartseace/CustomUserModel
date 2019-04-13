53264c2304dbab56899f8b5f08e3e38539a64336
karma <karmatseringhyolmo@gmail.com>
2019-04-13 07:29:36 +0545

    my custom usermodel

--- MyProject/users/views.py
+++ MyProject/users/views.py
@@ -1,5 +1,28 @@
-from django.shortcuts import render
+from django.contrib.auth import login,get_user_model,logout
+from django.shortcuts import render,HttpResponseRedirect
+from . forms import UserCreationForm,UserLoginForm
 
 # Create your views here.
-def register(request,):
-    formo=UserCreationForm
+def register(request, *args, **kwargs):
+    form=UserCreationForm(request.POST or None )
+    if form.is_valid():
+        form.save()
+        return HttpResponseRedirect('/login')
+
+    context={
+        'form':form
+    }
+    return render(request, "registration/register.html",context)
+
+def login_view(request, *args, **kwargs):
+    form = UserLoginForm(request.POST or None)
+    if form.is_valid():
+        user_obj = form.cleaned_data.get('user_obj')
+        login(request, user_obj)
+        return HttpResponseRedirect("/")
+    return render(request, "registration/login.html", {"form": form})
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect("/login")
